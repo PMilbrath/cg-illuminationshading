105c31a1f5abeb76e8ed18589eabf8138dbe7d16
ande8036 <71401604+ande8036@users.noreply.github.com>
2022-05-03 22:43:24 -0500

    Custom Model Made

--- scripts/models.js
+++ scripts/models.js
@@ -395,5 +395,460 @@ function createSphereVertexArray(gl, position_attrib, normal_attrib, texcoord_at
 //         - minimum of 16 vertices
 //
 function createCustomVertexArray(gl, position_attrib, normal_attrib, texcoord_attrib) {
-    return null;
+    // create a new Vertex Array Object
+    let vertex_array = gl.createVertexArray();
+    // set newly created Vertex Array Object as the active one we are modifying
+    gl.bindVertexArray(vertex_array);
+
+    
+    // create buffer to store vertex positions (3D points)
+    let vertex_position_buffer = gl.createBuffer();
+    // set newly created buffer as the active one we are modifying
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_position_buffer);
+    // create array of 3D vertex values (each set of 3 values specifies a vertex: x, y, z)
+    let vertices = [
+        // Front face
+        -0.5, -0.5,  0.5,
+            0.5, -0.5,  0.5,
+            0.5,  0.5,  0.5,
+        -0.5,  0.5,  0.5,
+
+        // Back face
+            0.5, -0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5,  0.5, -0.5,
+            0.5,  0.5, -0.5,
+
+        // Top face
+        -0.5,  0.5,  0.5,
+            0.5,  0.5,  0.5,
+            0.5,  0.5, -0.5,
+        -0.5,  0.5, -0.5,
+
+        // Bottom face
+            0.5, -0.5,  0.5,
+        -0.5, -0.5,  0.5,
+        -0.5, -0.5, -0.5,
+            0.5, -0.5, -0.5,
+
+        // Right face
+            0.5, -0.5,  0.5,
+            0.5, -0.5, -0.5,
+            0.5,  0.5, -0.5,
+            0.5,  0.5,  0.5,
+
+        // Left face
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5,  0.5,
+        -0.5,  0.5,  0.5,
+        -0.5,  0.5, -0.5,
+
+        // left roof
+        -0.5, 0.5, 0.5, //bottom front pixel
+        0.0, 1.0, 0.5, //top front pixel
+        0.0, 1.0, -0.5, //top back pixel
+        -0.5, 0.5, -0.5,
+
+        // right roof
+        0.5, 0.5, 0.5, //bottom front pixel
+        0.0, 1.0, 0.5, //top front pixel
+        0.0, 1.0, -0.5, //top back pixel
+        0.5, 0.5, -0.5,
+
+        // front "roof"
+        -0.5, 0.5, 0.5,
+        0.0, 1.0, 0.5,
+        0.5, 0.5, 0.5,
+        -0.5, 0.5, 0.5,
+
+        // back "roof"
+        -0.5, 0.5, -0.5,
+        0.0, 1.0, -0.5,
+        0.5, 0.5, -0.5,
+        -0.5, 0.5, -0.5,
+
+        // door face
+        -0.1, -0.5,  0.6,
+        0.1, -0.5,  0.6,
+        0.1,  -0.1,  0.6,
+        -0.1,  -0.1,  0.6,
+
+        // door top
+        -0.1, -0.1, 0.6,
+        0.1, -0.1, 0.6,
+        0.1, -0.1, 0.5,
+        -0.1, -0.1, 0.5,
+
+        // door bottom
+        -0.1, -0.5, 0.6,
+        0.1, -0.5, 0.6,
+        0.1, -0.5, 0.5,
+        -0.1, -0.5, 0.5,
+
+        // door left
+        -0.1, -0.1, 0.6,
+        -0.1, -0.1, 0.5,
+        -0.1, -0.5, 0.5,
+        -0.1, -0.5, 0.6,
+
+        // door right
+        0.1, -0.1, 0.6,
+        0.1, -0.1, 0.5,
+        0.1, -0.5, 0.5,
+        0.1, -0.5, 0.6,
+
+        // handle face
+        0.01, -0.27,  0.65,
+        0.09, -0.27,  0.65,
+        0.09,  -0.3,  0.65,
+        0.01,  -0.3,  0.65,
+
+        // handle top
+        0.01, -0.27, 0.65,
+        0.09, -0.27, 0.65,
+        0.09, -0.27, 0.6,
+        0.01, -0.27, 0.6,
+
+        // handle bottom
+        0.01, -0.3, 0.65,
+        0.09, -0.3, 0.65,
+        0.09, -0.3, 0.6,
+        0.01, -0.3, 0.6,
+
+        // handle left
+        0.01, -0.27, 0.65,
+        0.01, -0.27, 0.6,
+        0.01, -0.3, 0.6,
+        0.01, -0.3, 0.65,
+
+        // handle right
+        0.09, -0.27, 0.65,
+        0.09, -0.27, 0.6,
+        0.09, -0.3, 0.6,
+        0.09, -0.3, 0.65
+
+    ];
+    // store array of vertex positions in the vertex_position_buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    // enable position_attrib in our GPU program
+    gl.enableVertexAttribArray(position_attrib);
+    // attach vertex_position_buffer to the position_attrib
+    // (as 3-component floating point values)
+    gl.vertexAttribPointer(position_attrib, 3, gl.FLOAT, false, 0, 0);
+
+
+    // create buffer to store vertex normals (vector pointing perpendicular to surface)
+    let vertex_normal_buffer = gl.createBuffer();
+    // set newly created buffer as the active one we are modifying
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_normal_buffer);
+    // create array of 3D vector values (each set of 3 values specifies a normalized vector: x, y, z)
+    let normals = [
+        // Front
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+
+        // Back
+        0.0,  0.0, -1.0,
+        0.0,  0.0, -1.0,
+        0.0,  0.0, -1.0,
+        0.0,  0.0, -1.0,
+
+        // Top
+        0.0,  1.0,  0.0,
+        0.0,  1.0,  0.0,
+        0.0,  1.0,  0.0,
+        0.0,  1.0,  0.0,
+
+        // Bottom
+        0.0, -1.0,  0.0,
+        0.0, -1.0,  0.0,
+        0.0, -1.0,  0.0,
+        0.0, -1.0,  0.0,
+
+        // Right
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+
+        // Left
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+
+        //left roof
+        -0.5, 0.5, 0.0,
+        -0.5, 0.5, 0.0,
+        -0.5, 0.5, 0.0,
+        -0.5, 0.5, 0.0,
+
+        //right roof
+        0.5, 0.5, 0.0,
+        0.5, 0.5, 0.0,
+        0.5, 0.5, 0.0,
+        0.5, 0.5, 0.0,
+
+        //front "roof"
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0, 0.0, 1.0,
+
+        //back "roof"
+        0.0,  0.0,  -1.0,
+        0.0,  0.0,  -1.0,
+        0.0,  0.0,  -1.0,
+        0.0, 0.0, -1.0,
+
+        //door face
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+
+        //door top
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+
+        //door bottom
+        0.0, -1.0, 0.0,
+        0.0, -1.0, 0.0,
+        0.0, -1.0, 0.0,
+        0.0, -1.0, 0.0,
+
+        //door left
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+
+        //door right
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+
+        //handle face
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+
+        //handle top
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+
+        //handle bottom
+        0.0, -1.0, 0.0,
+        0.0, -1.0, 0.0,
+        0.0, -1.0, 0.0,
+        0.0, -1.0, 0.0,
+
+        //handle left
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+
+        //handle right
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+
+
+    ];
+    // store array of vertex normals in the vertex_normal_buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+    // enable normal_attrib in our GPU program
+    gl.enableVertexAttribArray(normal_attrib);
+    // attach vertex_normal_buffer to the normal_attrib
+    // (as 3-component floating point values)
+    gl.vertexAttribPointer(normal_attrib, 3, gl.FLOAT, false, 0, 0);
+
+
+    // create buffer to store texture coordinate (2D coordinates for mapping images to the surface)
+    let vertex_texcoord_buffer = gl.createBuffer();
+    // set newly created buffer as the active one we are modifying
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_texcoord_buffer);
+    // create array of 2D texture coordinate values (each set of 2 values specifies texture coordinate: u, v)
+    let texcoords = [
+        // Front
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        // Back
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        // Top
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        // Bottom
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        // Right
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        // Left
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        // Left roof
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        // Right roof
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        // front "roof"
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0, 1.0,
+
+        // back "roof"
+        0.0,  0.0,
+        1.0,  1.0,
+        1.0,  1.0,
+        0.0, 1.0,
+
+        // door face
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        //door top
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        //door bottom
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        //door left
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        //door right
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        // handle face
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        //handle top
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        //handle bottom
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        //handle left
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+
+        //handle right
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0
+    ];
+    // store array of vertex texture coordinates in the vertex_texcoord_buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texcoords), gl.STATIC_DRAW);
+    // enable texcoord_attrib in our GPU program
+    gl.enableVertexAttribArray(texcoord_attrib);
+    // attach vertex_texcoord_buffer to the texcoord_attrib
+    // (as 2-component floating point values)
+    gl.vertexAttribPointer(texcoord_attrib, 2, gl.FLOAT, false, 0, 0);
+
+    
+    // create buffer to store faces of the triangle
+    let vertex_index_buffer = gl.createBuffer();
+    // set newly created buffer as the active one we are modifying
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertex_index_buffer);
+    // create array of vertex indices (each set of 3 represents a triangle)
+    let indices = [
+            0,  1,  2,      0,  2,  3,   // Front
+            4,  5,  6,      4,  6,  7,   // Back
+            8,  9, 10,      8, 10, 11,   // Top
+        12, 13, 14,     12, 14, 15,   // Bottom
+        16, 17, 18,     16, 18, 19,   // Right
+        20, 21, 22,     20, 22, 23,    // Left
+        24, 25, 26,     24, 26, 27,    // Left Roof
+        28, 29, 30,     28, 30, 31,     // Right Roof
+        32, 33, 34,     32, 34, 35,     // Front "Roof"
+        36, 37, 38,     36, 38, 39,      // Back "Roof"
+        40, 41, 42,     40, 42, 43,      // Door Face
+        44, 45, 46,     44, 46, 47,      // Door Top
+        48, 49, 50,     48, 50, 51,      //Door Bottom
+        52, 53, 54,     52, 54, 55,      //Door Left
+        56, 57, 58,     56, 58, 59,     //Door Right
+        60, 61, 62,     60, 62, 63,      //Handle Face
+        64, 65, 66,     64, 66, 67,      //Handle Top
+        68, 69, 70,     68, 70, 71,      //Handle Bottom
+        72, 73, 74,     72, 74, 75,      //Handle Left
+        76, 77, 78,     76, 78, 79      //Handle Right
+
+    ];
+    // store array of vertex indices in the vertex_index_buffer
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+
+    // no longer modifying our Vertex Array Object, so deselect
+    gl.bindVertexArray(null);
+
+
+    // store the number of vertices used for entire model (number of faces * 3)
+    vertex_array.face_index_count = indices.length;
+
+
+    // return created Vertex Array Object
+    return vertex_array;
 }
